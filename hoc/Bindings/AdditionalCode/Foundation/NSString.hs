92f17e296df420a8ed7fd91f07fc3a20390f9683
wthaller <wthaller@587db195-be3e-0410-add0-45d45444aa60>
2003-11-02 10:45:16 +0000

    String Marshalling outside the IO monad
    git-svn-id: http://hoc.googlecode.com/svn/trunk@41 587db195-be3e-0410-add0-45d45444aa60

--- hoc/Bindings/AdditionalCode/Foundation/NSString.hs
+++ hoc/Bindings/AdditionalCode/Foundation/NSString.hs
@@ -1,19 +1,40 @@
 --X haskellString
 --X stringWithHaskellString
 --X nsString
+--X toNSString
+--X fromNSString
 
 import Foreign.C.String
-import Foundation.NSObject(alloc)
+import Foundation.NSObject  ( alloc )
+import System.IO.Unsafe     ( unsafePerformIO )
 -- CUT HERE
 
 haskellString :: NSString a -> IO String
+nsString :: String -> IO (NSString ())
 stringWithHaskellString :: String -> NSStringClass a -> IO (NSString a)
 
-haskellString nsstr = do
-    utf8 <- nsstr # utf8String
-    peekCString utf8
-    
-stringWithHaskellString str cls = do
-    withCString str (\cstr -> cls #* alloc >>= initWithUTF8String cstr)
+    -- export and reimport as the other type
+haskellString nsstr = withExportedArgument nsstr importArgument
+nsString nsstr = withExportedArgument nsstr importArgument
 
-nsString str = _NSString # stringWithHaskellString str
+    -- and for completeness...
+stringWithHaskellString str cls = do
+    withCString str (\cstr -> cls # alloc >>= initWithUTF8String cstr)
+
+-- PURE variants:
+
+-- NSString is immutable, so we may do somethings outside the IO monad.
+
+-- If a NSString has been created using toNSString, it should never be compared
+-- using (==), because that yields undefined results.
+ 
+toNSString :: String -> NSString ()
+toNSString = unsafePerformIO . nsString
+
+-- fromNSString does not accept a subclass of NSString as an argument,
+-- because subclasses can be mutable.
+-- You should always make sure that you're not in fact passing a (casted)
+-- NSMutableString.
+
+fromNSString :: NSString () -> String
+fromNSString = unsafePerformIO . haskellString
