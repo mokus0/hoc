16b0a40742fd28acc8e7b7f893d732b1aedff2bd
wthaller <wthaller@587db195-be3e-0410-add0-45d45444aa60>
2004-02-24 22:16:50 +0000

    Add nsApplicationMain and nsApplicationMain_ (a version without arguments) to module NSApplication
    git-svn-id: http://hoc.googlecode.com/svn/trunk@56 587db195-be3e-0410-add0-45d45444aa60

---
+++ hoc/Bindings/AdditionalCode/AppKit/NSApplication.hs
@@ -0,0 +1,22 @@
+--X nsApplicationMain
+--X nsApplicationMain_
+
+import Foreign            ( withMany, withArray0, Ptr )
+import Foreign.C          ( withCString, CInt, CString )
+import System.Environment ( getProgName, getArgs )
+
+-- CUT HERE
+
+
+foreign import ccall "NSApplicationMain" c_nsApplicationMain
+	:: CInt -> Ptr CString -> IO CInt
+
+nsApplicationMain prog args =
+	withMany withCString (prog : args) $ \argvPtrs ->
+	withArray0 nullPtr argvPtrs $ \argvBuf ->
+	c_nsApplicationMain (1 + (fromIntegral $ length args)) argvBuf
+
+nsApplicationMain_ = do
+    prog <- getProgName
+    args <- getArgs
+    nsApplicationMain prog args
