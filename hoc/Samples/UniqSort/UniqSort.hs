acbed8d55e9b63064424396c2fe9f8d3c129f6f7
athp <athp@587db195-be3e-0410-add0-45d45444aa60>
2004-05-07 08:05:27 +0000

    Added UniqSort sample
    git-svn-id: http://hoc.googlecode.com/svn/trunk@79 587db195-be3e-0410-add0-45d45444aa60

---
+++ hoc/Samples/UniqSort/UniqSort.hs
@@ -0,0 +1,58 @@
+module Main where
+
+import ArrayHelper
+
+import List (genericLength)
+import System
+
+import Foundation
+import Foundation.NSProcessInfo (arguments)
+import HOC
+import HOC.Base (newAutoreleasePool, releaseObject)
+import HOC.ID (isNil)
+
+-- int main (int argc, const char * argv[]) {
+main = do
+--   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+     pool <- newAutoreleasePool
+--   NSArray *args = [[[NSProcessInfo processInfo] arguments];
+     args <- _NSProcessInfo # processInfo >>= arguments
+--   NSCountedSet *cset = [[NSCountedSet alloc] initWithArray:args];
+     cset <- _NSCountedSet # alloc >>= initWithArray args
+--   NSArray *sorted_args = [[cset allObjects]
+--     sortedArrayUsingSelector:@selector(compare:)];
+     sorted_args <- cset # allObjects
+       >>= sortedArrayUsingSelector (getSelectorForName "compare:")
+--   NSEnumerator *enm = [sorted_args objectEnumerator];
+     enm <- sorted_args # objectEnumerator
+--   id word;
+--   while (word = [enm nextObject]) {
+--     printf("%s\n", [word UTF8String]);
+--   }
+     let loop = do
+	 word <- enm # nextObject
+	 if (not (isNil word))
+	   then do { putStrLn ( (fromNSString.castObject) word ); loop }
+	   else do return ()
+         in
+             loop
+--   [cset release];
+     -- Don't need to release anything: HOC manages memory for you via
+     -- Haskell's garbage collector!
+--   [pool release];
+     releaseObject pool
+--   return 0;
+     return ()
+-- }
+
+
+{-
+   Note that an alternative Haskell implementation would have been:
+
+     main = do
+       words <- System.getArgs
+       mapM_ putStrLn ( (sort.nub) words )
+
+   ... which is a bit shorter and neater. :)
+-}
+
