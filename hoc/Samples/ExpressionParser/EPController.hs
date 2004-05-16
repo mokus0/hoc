c0c7485da356843108a807712b5d81dbea3d9f09
athp <athp@587db195-be3e-0410-add0-45d45444aa60>
2004-05-16 08:47:34 +0000

    Renamed "evaluate" selector to "evaluteExpression" (to avoid conflicts with existing Cocoa names)
    Added ExpressionParser.nib file
    
    
    git-svn-id: http://hoc.googlecode.com/svn/trunk@111 587db195-be3e-0410-add0-45d45444aa60

--- hoc/Samples/ExpressionParser/EPController.hs
+++ hoc/Samples/ExpressionParser/EPController.hs
@@ -2,29 +2,34 @@
 
 module EPController where
 
-import Cocoa hiding (parse, info_evaluate, ImpType_evaluate)
+import Cocoa hiding (parse)
 import ExpressionParser
 import Selectors
 import Text.ParserCombinators.Parsec (parse)
+
+import GHC.Ptr
+import HOC.Base
 
 $(declareClass "EPController" "NSObject")
 
 $(exportClass "EPController" "ep_"
   [ Outlet "expressionEntry" [t| NSTextField () |]
   , Outlet "evaluation"      [t| NSTextField () |]
-  , InstanceMethod Selectors.info_evaluate
+  , InstanceMethod Selectors.info_evaluateExpression
   ]
  )
 
 obj #. var = obj # getIVar var
 
-ep_evaluate _ self = do
+ep_evaluateExpression _ self = do
   expressionTextField <- (self #. _expressionEntry)
   expression <- expressionTextField # stringValue >>= haskellString
   evaluation <- self #. _evaluation
   case (parse expr "" expression) of
     Left err ->
-      self #. _evaluation >>= setStringValue (toNSString "(error)")
+      self #. _evaluation >>=
+        setStringValue (toNSString $ "Error " ++ show err)
     Right answer ->
-      self #. _evaluation >>= setStringValue (toNSString $ show answer)
+      self #. _evaluation >>=
+        setStringValue (toNSString $ show answer)
 
--- hoc/Samples/ExpressionParser/Selectors.hs
+++ hoc/Samples/ExpressionParser/Selectors.hs
@@ -4,5 +4,5 @@ module Selectors where
 
 import AppKit.NSButton
 
-$(declareSelector "evaluate:" [t| forall a. NSButton a -> IO () |])
+$(declareSelector "evaluateExpression:" [t| forall a. NSButton a -> IO () |])
 
