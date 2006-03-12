6b5ea7bfd6ff9c69529ea95eed27036cf26a7117
wthaller <wthaller@587db195-be3e-0410-add0-45d45444aa60>
2006-03-12 18:46:11 +0000

    add NSTimeInterval type
    git-svn-id: http://hoc.googlecode.com/svn/trunk@242 587db195-be3e-0410-add0-45d45444aa60

---
+++ hoc/Bindings/AdditionalCode/Foundation/NSDate.hs
@@ -0,0 +1,15 @@
+-- above NSDate
+-- CUT HERE
+-- below NSDate
+-- CUT HERE
+-- above NSDate.Forward
+
+--X NSTimeInterval
+
+
+-- CUT HERE
+
+-- below NSDate.Forward
+
+type NSTimeInterval = Double
+
