51004d042cceb7197a969d82e1126856b0ab7012
wolfgang.thaller@gmx.net <wolfgang.thaller@gmx.net@587db195-be3e-0410-add0-45d45444aa60>
2007-12-30 18:45:36 +0000

    comment outdated instance definition (which is now in the TH library)
    git-svn-id: http://hoc.googlecode.com/svn/trunk@271 587db195-be3e-0410-add0-45d45444aa60

--- hoc/HOC/HOC/TH.hs
+++ hoc/HOC/HOC/TH.hs
@@ -11,8 +11,8 @@ module HOC.TH(
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 
-instance Functor Q where
-    fmap f q = q >>= return . f
+-- instance Functor Q where
+--    fmap f q = q >>= return . f
 
 whereQ :: ([Q Dec] -> Q a) -> Q [Dec] -> Q a
 header `whereQ` declsQ = do
