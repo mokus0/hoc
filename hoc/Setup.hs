5fc5cace1e1fd19b67c7500008194b1a220ffe17
wolfgang.thaller@gmx.net <wolfgang.thaller@gmx.net@587db195-be3e-0410-add0-45d45444aa60>
2008-01-03 22:46:00 +0000

    Fix some things about Setup.hs
    git-svn-id: http://hoc.googlecode.com/svn/trunk@280 587db195-be3e-0410-add0-45d45444aa60

--- hoc/Setup.hs
+++ hoc/Setup.hs
@@ -12,24 +12,18 @@ main = defaultMainWithHooks $ simpleUserHooks {
 
 customPreBuild :: Args -> BuildFlags -> IO HookedBuildInfo
 customPreBuild args buildFlags = do
-    putStrLn "preBuild"
-    -- print buildFlags
-    -- print $ case parseHookedBuildInfo "ghc-options: H.o" of ParseOk _ x -> x
-    --let (Just y, []) = case parseHookedBuildInfo "ghc-options: H.o" of ParseOk _ x -> x
-    
-    -- return (Nothing, [("ATest", y)])
-    -- return (Just y, [])
-    --return emptyHookedBuildInfo
-    
-    exitCode <- system "gcc -gstabs+ -r -nostdlib -I`ghc --print-libdir`/include -DMACOSX HOC_cbits/*.m -o dist/build/HOC_cbits.o"
+    putStrLn "Compiling HOC_cbits..."
+    system "mkdir dist/build/"
+    exitCode <- system "gcc -r -nostdlib -I`ghc --print-libdir`/include -DMACOSX HOC_cbits/*.m -o dist/build/HOC_cbits.o"
     
     case exitCode of
         ExitSuccess -> return ()
         _ -> fail "Failed in C compilation."
     
     let buildInfo = emptyBuildInfo {
-            options = [ (GHC, ["dist/build/HOC_cbits/HOC_cbits.o",
+            options = [ (GHC, ["dist/build/HOC_cbits.o",
                                "-lobjc",
+                               "-lffi",
                                "-framework Foundation"]) ],
             cSources = ["HOC_cbits.o"]
         }
