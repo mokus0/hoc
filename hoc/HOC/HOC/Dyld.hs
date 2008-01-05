cb23591eb87268cc720e48c4c2eea486976585b9
wolfgang.thaller@gmx.net <wolfgang.thaller@gmx.net@587db195-be3e-0410-add0-45d45444aa60>
2008-01-05 20:16:40 +0000

    Remove an old workaround for a Mac OS specific bug in the binding to the dynamic linker. For a long time now, System.Posix.DynamicLinker has been perfectly fit for general consumption.
    git-svn-id: http://hoc.googlecode.com/svn/trunk@283 587db195-be3e-0410-add0-45d45444aa60

--- hoc/HOC/HOC/Dyld.hs
+++ hoc/HOC/HOC/Dyld.hs
@@ -6,16 +6,11 @@ module HOC.Dyld(
 import GHC.Exts(Ptr(..), Addr#)
 import Foreign
 import Foreign.C.String
-
-    -- Up to GHC 6.4.0, there was a bug where rtldDefault was marshalled
-    -- incorrectly for Mac OS X, so we do it by hand.
-
-foreign import ccall "dlsym" c_dlsym :: Ptr () -> CString -> IO (FunPtr a)
-
-rtldDefault = nullPtr `plusPtr` (-2)
+import System.Posix.DynamicLinker
 
 lookupSymbol :: String -> IO (FunPtr a)
 lookupSymbol# :: Addr# -> IO (FunPtr a)
 
-lookupSymbol name = withCString name (c_dlsym rtldDefault)
-lookupSymbol# name# = c_dlsym rtldDefault (Ptr name#)
+lookupSymbol = dlsym Default
+lookupSymbol# name# = c_dlsym (packDL Default) (Ptr name#)
+
