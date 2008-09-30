55528f44cf1185343557651f4e00562caf989805
wolfgang.thaller@gmx.net <wolfgang.thaller@gmx.net@587db195-be3e-0410-add0-45d45444aa60>
2008-09-30 22:43:17 +0000

    more cleanup in binding generator scripts
    git-svn-id: http://hoc.googlecode.com/svn/trunk@323 587db195-be3e-0410-add0-45d45444aa60

--- hoc/Bindings/just-generate.sh
+++ hoc/Bindings/just-generate.sh
@@ -1,24 +1,15 @@
-function build()
-{
-	true
-}
-
 ARGUMENTS=$*
+OPTS=-q
 
-IFGEN=hoc-ifgen #"../../InterfaceGenerator2/hoc-ifgen"
+IFGEN=hoc-ifgen
 
+set -e
 mkdir -p Generated
 cd Generated
 
-OPTS=-q
 $IFGEN Foundation -f -b ../binding-script.txt -a ../AdditionalCode/ $OPTS
-build Foundation
 $IFGEN QuartzCore -f -b ../binding-script.txt -a ../AdditionalCode/ -d Foundation $OPTS
-build QuartzCore
 $IFGEN AppKit -f -b ../binding-script.txt -a ../AdditionalCode/ -d Foundation -d QuartzCore $OPTS
-build AppKit
 $IFGEN CoreData -f -b ../binding-script.txt -a ../AdditionalCode/ -d Foundation \
     -d AppKit -d QuartzCore $OPTS    # fake dependencies
-build CoreData
 $IFGEN Cocoa -u -d Foundation -d QuartzCore -d AppKit -d CoreData $OPTS
-build Cocoa
--- hoc/Bindings/make-bindings-macos.sh
+++ hoc/Bindings/make-bindings-macos.sh
@@ -10,19 +10,20 @@ function build()
 ARGUMENTS=$*
 OPTS=
 
+IFGEN=hoc-ifgen
+
 set -e
 mkdir -p Generated
 cd Generated
 
-
-hoc-ifgen Foundation -f -b ../binding-script.txt -a ../AdditionalCode/ $OPTS
-build HOC-Foundation
-hoc-ifgen QuartzCore -f -b ../binding-script.txt -a ../AdditionalCode/ -d Foundation $OPTS
-build HOC-QuartzCore
-hoc-ifgen AppKit -f -b ../binding-script.txt -a ../AdditionalCode/ -d Foundation -d QuartzCore $OPTS
-build HOC-AppKit
-hoc-ifgen CoreData -f -b ../binding-script.txt -a ../AdditionalCode/ -d Foundation \
-    -d AppKit -d QuartzCore  $OPTS    # fake dependencies
-build HOC-CoreData
-hoc-ifgen Cocoa -u -d Foundation -d QuartzCore -d AppKit -d CoreData  $OPTS
-build HOC-Cocoa
+$IFGEN Foundation -f -b ../binding-script.txt -a ../AdditionalCode/ $OPTS
+build Foundation
+$IFGEN QuartzCore -f -b ../binding-script.txt -a ../AdditionalCode/ -d Foundation $OPTS
+build QuartzCore
+$IFGEN AppKit -f -b ../binding-script.txt -a ../AdditionalCode/ -d Foundation -d QuartzCore $OPTS
+build AppKit
+$IFGEN CoreData -f -b ../binding-script.txt -a ../AdditionalCode/ -d Foundation \
+    -d AppKit -d QuartzCore $OPTS    # fake dependencies
+build CoreData
+$IFGEN Cocoa -u -d Foundation -d QuartzCore -d AppKit -d CoreData $OPTS
+build Cocoa
