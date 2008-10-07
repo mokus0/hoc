d78f213f8ffdfdfdfc435226a43097bac8b31612
wolfgang.thaller@gmx.net <wolfgang.thaller@gmx.net@587db195-be3e-0410-add0-45d45444aa60>
2008-10-07 22:14:47 +0000

    Make enum names hack less aggressive -- do not combine anonymous enums with int (long/unsigned/etc.) typedefs that follow.
    git-svn-id: http://hoc.googlecode.com/svn/trunk@329 587db195-be3e-0410-add0-45d45444aa60

--- hoc/InterfaceGenerator2/HackEnumNames.hs
+++ hoc/InterfaceGenerator2/HackEnumNames.hs
@@ -6,15 +6,14 @@ import Headers
 
 hackEnumNames :: HeaderInfo -> HeaderInfo
 
-hackEnumNames (HeaderInfo name imports decls)
-    = HeaderInfo name imports (hackEnums1 Just id decls)
+hackEnumNames (HeaderInfo headerName imports decls)
+    = HeaderInfo headerName imports (hackEnums1 Just id decls)
     where
         hackEnums1 :: (a -> Maybe DeclarationAndPos) -> (DeclarationAndPos -> a) -> [a] -> [a]
         hackEnums1 unwrap wrap (x : y : xs)
             | Just (pos, CTypeDecl (CTEnum name1 vals)) <- unwrap x,
               Just (_, Typedef baseType name2) <- unwrap y,
-              null name1 || name1 == name2 || name1 == '_' : name2,
-              acceptableEnumBaseType baseType
+              null name1 && acceptableEnumBaseType name2 baseType || acceptableEnumTag name1 name2
             = wrap (pos, Typedef (CTEnum name1 vals) name2)
                 : hackEnums1 unwrap wrap xs
         hackEnums1 unwrap wrap (x : xs)
@@ -28,8 +27,9 @@ hackEnumNames (HeaderInfo name imports decls)
                   decl (pos, d) = (pos, LocalDecl d)
         hackEnums1 unwrap wrap [] = []
         
-        acceptableEnumBaseType (CTSimple name)
+        acceptableEnumBaseType name2 (CTSimple name)
             | name == "NSInteger" || name == "NSUInteger" = True
-        acceptableEnumBaseType (CTBuiltin _ _ name)
-            | name == "int" = True
-        acceptableEnumBaseType _ = False
+        acceptableEnumBaseType _ _ = False
+
+        acceptableEnumTag name1 name2
+            = dropWhile (== '_') name1 == name2
