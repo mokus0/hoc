6a309b76b0d4e3422ae0810d3beb3df7156e9b4c
James Cook <mokus@deepbondi.net>
2012-04-24 16:24:26 -0400

    Minor layout changes

--- hoc-cbits/src/HOC/CBits/Types.hs
+++ hoc-cbits/src/HOC/CBits/Types.hs
@@ -6,7 +6,14 @@ import Foreign.Ptr
 import Foreign.LibFFI.Experimental
 import Foreign.ObjC
 
-type HsIMP a = CIF (Ptr ObjCObject -> SEL a -> a) -> Ptr (SigReturn a) -> Ptr (Ptr ()) -> IO ObjCException
-foreign import ccall "wrapper" wrapHsIMP :: HsIMP a -> IO (FunPtr (HsIMP a))
-foreign import ccall newIMP :: CIF (Ptr ObjCObject -> SEL a -> a) -> FunPtr (HsIMP a) -> IO (IMP a)
+type HsIMP a = 
+        CIF (Ptr ObjCObject -> SEL a -> a)
+     -> Ptr (SigReturn a)
+     -> Ptr (Ptr ())
+     -> IO ObjCException
 
+foreign import ccall "wrapper"
+    wrapHsIMP :: HsIMP a -> IO (FunPtr (HsIMP a))
+
+foreign import ccall
+    newIMP :: CIF (Ptr ObjCObject -> SEL a -> a) -> FunPtr (HsIMP a) -> IO (IMP a)
