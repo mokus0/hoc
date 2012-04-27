be1ef3b378c92cdfa7f79ab9f62f66613d16f3d1
James Cook <mokus@deepbondi.net>
2012-04-27 09:51:45 -0400

    ifgen: remove TH-based traversal generation code

--- ifgen/src/Traversals.hs
+++ ifgen/src/Traversals.hs
@@ -1,63 +1,26 @@
-{-# LANGUAGE CPP, TemplateHaskell, RelaxedPolyRec #-}
 module Traversals where
-
-#define TEMPLATES 1
 
 import Entities
 import Control.Exception
 
-#if TEMPLATES
-import THTraversal
-#else
 import Data.Generics
-#endif
 
 mapEntityIDs :: (EntityID -> EntityID) -> Entity -> Entity
-#if TEMPLATES
-mapEntityIDs f =
-    $(makeEverywhere [t| Entity |]
-                [ ( [t| EntityID |], [| f |] ) ])
-#else
 mapEntityIDs f = everywhere (mkT f)
-#endif
 
 mapEntityIDsAndModules :: (EntityID -> EntityID)
                        -> (Module -> Module)
                        -> Entity -> Entity
-
-#if TEMPLATES
-mapEntityIDsAndModules f g
-    = $(makeEverywhere [t| Entity |]
-                [ ( [t| EntityID |], [| f |] ),
-                  ( [t| Module |],   [| g |] ) ])
-#else
 mapEntityIDsAndModules f g = everywhere (mkT f `extT` g)
-#endif
 
 
 mapEntityIDsInEntity :: (EntityID -> EntityID) -> Entity -> Entity
-#if TEMPLATES
-mapEntityIDsInEntity f
-    = $(makeEverywhere [t| Entity |]
-                [ ( [t| EntityID |], [| f |] ) ])
-#else
 mapEntityIDsInEntity f = everywhere (mkT f)
-#endif
-
 
 mapTypes :: (HaskellValueType -> HaskellValueType)
          -> (HaskellSelectorType -> HaskellSelectorType)
          -> Entity -> Entity
-
-#if TEMPLATES
-mapTypes val sel
-    = $(makeEverywhere [t| Entity |]
-                [ ( [t| HaskellValueType |], [| val |] ),
-                  ( [t| HaskellSelectorType |], [| sel |] ) ])
-#else
 mapTypes val sel = everywhere (mkT val `extT` sel)
-#endif
-
 
 foreachTypeInEntity :: Monad m
                     => (HaskellValueType -> m ())
@@ -65,13 +28,6 @@ foreachTypeInEntity :: Monad m
                     -> Entity
                     -> m ()
 
-#if TEMPLATES
-foreachTypeInEntity val sel
-    = $(makeForeachM [t| Entity |]
-        [ ([t| HaskellValueType |], [| val |]),
-          ([t| HaskellSelectorType |], [| sel |]) ])
-
-#else
 foreachTypeInEntity val sel e
     = do
         everywhereM (mkM (\x -> sel x >> return x)
@@ -79,51 +35,19 @@ foreachTypeInEntity val sel e
                     e
         return ()
 
-#endif
-
 foreachIDInEntity :: Monad m
                   => (EntityID -> m ())
               -> Entity
               -> m ()
-#if TEMPLATES
-foreachIDInEntity f
-    = $(makeForeachM [t| Entity |]
-        [ ([t| EntityID |], [| f |]) ])
-
-#else
 foreachIDInEntity f e
     = do
         everywhereM (mkM (\x -> f x >> return x))
                     e
         return ()
 
-#endif
-
 mentionedEntityIDs :: Entity -> [EntityID]
-
-#if TEMPLATES
-mentionedEntityIDs
-    = $(makeTraverser [t| Entity |]
-                      [| const [] |]
-                      [| (++) |]
-                      [| const [] |]
-                      [ ( [t| EntityID |], [| \x -> [x] |] ) ]
-        )
-#else
 mentionedEntityIDs = listify (const True)
-#endif
 
 deepEvaluateEntity :: Entity -> IO ()
-
-#if TEMPLATES
-deepEvaluateEntity
-    = $(makeTraverser [t| Entity |]
-                      [| const (return ()) |]
-                      [| flip (>>) |]
-                      [| \x -> evaluate x >> return () |]
-                      [])
-#else
 deepEvaluateEntity x
     = everywhereM evaluate x >> return ()
-#endif
-
